855fc182b7eac7d73862dbf3e549f179364d43fa
Nikita <nikita.loginov.atvinta@gmail.com>
2024-01-25 13:36:30 +0300

    feat(Фронтенд|Страницы): Добавлены страницы проекта

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,77 @@
 <template>
-  <h1>Blog</h1>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/categories">Categories</router-link>
+  <nav class="navbar orange lighten-1">
+    <div class="nav-wrapper">
+      <div class="navbar-left">
+        <a href="#">
+          <i class="material-icons black-text">dehaze</i>
+        </a>
+        <span class="black-text">12.12.12</span>
+      </div>
+
+      <ul class="right hide-on-small-and-down">
+        <li>
+          <a
+              class="dropdown-trigger black-text"
+              href="#"
+              data-target="dropdown"
+          >
+            USER NAME
+            <i class="material-icons right">arrow_drop_down</i>
+          </a>
+
+          <ul id='dropdown' class='dropdown-content'>
+            <li>
+              <a href="#" class="black-text">
+                <i class="material-icons">account_circle</i>Профиль
+              </a>
+            </li>
+            <li class="divider" tabindex="-1"></li>
+            <li>
+              <a href="#" class="black-text">
+                <i class="material-icons">assignment_return</i>Выйти
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
-    <router-view />
+  </nav>
+
+  <div id="nav">
+    <ul class="sidenav app-sidenav open">
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/">На главную</router-link>
+      </li>
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/record">Счет</router-link>
+      </li>
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/history">История</router-link>
+      </li>
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/planning">Планирование</router-link>
+      </li>
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/categories">Категории</router-link>
+      </li>
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/login">Авторизация</router-link>
+      </li>
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/register">Регистрация</router-link>
+      </li>
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/profile">Профиль</router-link>
+      </li>
+    </ul>
+  </div>
+  <router-view/>
+
+  <div class="fixed-action-btn">
+    <a class="btn-floating btn-large blue" href="#">
+      <i class="large material-icons">add</i>
+    </a>
+  </div>
 </template>
 
 <style lang="scss">
--- app/src/router.js
+++ app/src/router.js
@@ -8,15 +8,40 @@ const routes = [
         component: Home,
     },
     {
+        path: '/register',
+        name: 'register',
+        component: () => import('@/views/RegisterView.vue'),
+    },
+    {
         path: '/login',
         name: 'login',
         component: () => import('@/views/LoginView.vue'),
     },
     {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('@/views/ProfileView.vue'),
+    },
+    {
         path: '/categories',
         name: 'categories',
         component: () => import('@/views/CategoryView.vue'),
-    }
+    },
+    {
+        path: '/history',
+        name: 'history',
+        component: () => import('@/views/HistoryView.vue'),
+    },
+    {
+        path: '/planning',
+        name: 'planning',
+        component: () => import('@/views/PlanningView.vue'),
+    },
+    {
+        path: '/record',
+        name: 'record',
+        component: () => import('@/views/RecordView.vue'),
+    },
 ]
 
 const router = createRouter({
