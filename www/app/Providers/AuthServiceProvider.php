c78a983ebcf3c687f455d3b6b358eeb9c5996b94
Nikita <nikita.loginov.atvinta@gmail.com>
2024-04-22 19:20:33 +0300

    Добавлены библиотеки для генерирования ide-helper, написания DTO и репозиториев

--- www/app/Providers/AuthServiceProvider.php
+++ www/app/Providers/AuthServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,6 +20,5 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Passport::hashClientSecrets();
     }
 }
